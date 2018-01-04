a3fe2f1a6bf85d52f9a81b31031617bf79f4efb7
jose nunes <josenunesra@gmail.com>
2018-01-03 23:06:56 -0200

    textos e gráficos

---
+++ arquivos_e_graficos/gerar_frames_animated_graph.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.linspace(0, 10, 100)
+y = np.sin(x)
+
+fig, ax = plt.subplots()
+line, = ax.plot(x, y, color='k')
+
+for n in range(len(x)):
+    line.set_data(x[:n], y[:n])
+    #ax.axis([0, 10, 0, 1])
+    fig.canvas.draw()
+    # dpi denota a qualidade da imagem (dots per inch)
+    plt.savefig('Frame%03d.png' %n, dpi=180)
---
+++ arquivos_e_graficos/plot_graph_from_CSV.py
@@ -0,0 +1,42 @@
+'''
+    Plotar gráfico a partir de dados em um arquivo CSV
+'''
+
+from matplotlib import pyplot as plt
+
+# ler arquivo CSV com os dados
+f = open('arquivos_texto/semperda.csv', 'r')
+x = list()
+y = list()
+for linha in f:
+    # é feito um split para seperar os dados, já que os dados são separados por ','
+    i = linha.split(',')
+    # converter cada valor para float e salvar em uma lista
+    x.append(float(i[0]))
+    y.append(float(i[1]))
+f.close()
+
+# definir uma função a plotar
+plt.scatter(x, y, label='Fronteira Nominal', alpha=0.5, marker='*', c='green')
+
+# definir o título dos eixos
+plt.xlabel('Fuel Cost $/h')
+plt.ylabel('Emission ton/h')
+
+# colocar legenda
+plt.legend(loc='best')
+
+# definir limites dos eixos
+#axes = plt.gca()
+#axes.set_xlim([597, 639])
+#axes.set_ylim([0.1875, 0.202])
+
+# outra maneira de definir os limites dos eixos
+plt.xlim(597, 639)
+plt.ylim(0.1875, 0.202)
+
+
+plt.show()
+
+#plt.savefig('grafico_python.svg')
+
