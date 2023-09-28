abbb7aaf907eda3a4875903799a4851cb3e8fc25
José Nunes <josenunesra@gmail.com>
2023-09-28 00:18:22 +0000

    feat(cache): basic example with lru_cache

---
+++ cache/examples.py
@@ -0,0 +1,20 @@
+# memoization
+# ensures that a function doesn’t run for the same inputs more than once by storing its result in memory and then referencing it later when necessary
+
+# lru_cache (Least Recent Used) is one implementation of it
+
+
+from functools import lru_cache
+
+
+@lru_cache(maxsize=None)
+def fib(n):
+    if n < 2:
+        return n
+    return fib(n-1) + fib(n-2)
+
+
+if __name__ == "__main__":
+    [fib(n) for n in range(20)]
+
+    print(fib.cache_info())
