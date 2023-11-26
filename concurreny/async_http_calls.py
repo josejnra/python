9943afb42c9fd5226171fd6a0eb9752932fec132
José Nunes <josenunesra@gmail.com>
2023-11-26 16:54:30 +0000

    feat(async): another example

--- concurreny/async_http_calls.py
+++ concurreny/async_http_calls.py
@@ -14,11 +14,15 @@ async def get_request_httpx(interaction: int, endpoint: str):
 
 async def endpoint_1():
     """httpx example"""
-    tasks = []
-    for interaction in range(10):
-        tasks.append(asyncio.create_task(get_request_httpx(interaction, "page-1")))
+    # tasks = []
+    # for interaction in range(1000):
+    #     tasks.append(asyncio.create_task(get_request_httpx(interaction, "page-1")))
 
-    await asyncio.gather(*tasks)
+    # await asyncio.gather(*tasks)
+
+    # suspend control over coroutines outside of this context
+    for interaction in range(1000):
+        await asyncio.ensure_future(get_request_httpx(interaction, "page-1"))
 
 
 async def get_request_aiohttp(interaction: int, endpoint: str):
@@ -30,11 +34,15 @@ async def get_request_aiohttp(interaction: int, endpoint: str):
 
 async def endpoint_2():
     """aiohttp example"""
-    tasks = []
-    for interaction in range(10):
-        tasks.append(asyncio.create_task(get_request_aiohttp(interaction, "page-2")))
+    # tasks = []
+    # for interaction in range(1000):
+    #     tasks.append(asyncio.create_task(get_request_aiohttp(interaction, "page-2")))
 
-    await asyncio.gather(*tasks)
+    # await asyncio.gather(*tasks)
+
+    # suspend control over coroutines outside of this context
+    for interaction in range(1000):
+        await asyncio.ensure_future(get_request_aiohttp(interaction, "page-2"))
 
 
 async def main():
@@ -42,4 +50,9 @@ async def main():
 
 
 if __name__ == "__main__":
+    from time import time
+
+    s = time()
     asyncio.run(main())
+    e = time()
+    print("execution time: ", e - s)
